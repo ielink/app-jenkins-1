82e68b18632c309d750051cc75330d7ef1fc81f3
hamid <hamid.shafikani@gmail.com>
2020-06-11 15:26:58 +0200

    rr

---
+++ src/test/java/com/example/appjenkins/ArtistServiceImplTest.java
@@ -0,0 +1,16 @@
+package com.example.appjenkins;
+
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class ArtistServiceImplTest {
+
+    @Test
+    public void testGetArtists() {
+
+        Assert.assertEquals("1", "1");
+    }
+
+
+}
