2836b8dc36703ee30f08029504a7bbed15aef39f
hamid <hamid.shafikani@gmail.com>
2020-07-08 23:39:35 +0200

    Add Jenkins file

--- Jenkinsfile
+++ Jenkinsfile
@@ -7,5 +7,10 @@ pipeline {
             echo $GIT_BRANCH
          }
       }
+      stage('Build') {
+               steps {
+                  echo $GIT_BRANCH
+               }
+            }
    }
 }
--- src/main/java/com/example/appjenkins/JenkinsController.java
+++ src/main/java/com/example/appjenkins/JenkinsController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class JenkinsController {
 
-    @GetMapping("/lazy v4")
+    @GetMapping("/lazy v5")
     public String getLazy() {
         return "lazy";
     }
