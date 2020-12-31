742141fdbab6114e05b9cf79af00f8df2a6842ca
hamid <hamid.shafikani@gmail.com>
2020-12-31 21:14:16 +0100

    add controller

--- helm/ingress/readme-helm.md
+++ helm/ingress/readme-helm.md
@@ -1,3 +1,8 @@
 # Overview
 this is the main helm chart with ingress
 with jenkinsfile
+
+* Run with JenkinsFile / build jar/ image/push docker/registry>>>
+* Run helm with tag
+* Go to `http://hello-world.pvt/swagger-ui.html#/`
+* Bingo
--- src/main/java/com/example/appjenkins/controller/DevOpsDemoController.java
+++ src/main/java/com/example/appjenkins/controller/DevOpsDemoController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.NoSuchElementException;
 
 @RestController
-@RequestMapping(value = "/employee")
+@RequestMapping(value = "/devops")
 @CrossOrigin(origins = "*")
 public class DevOpsDemoController {
 
@@ -13,6 +13,15 @@ public class DevOpsDemoController {
     public String getOrdersById(@PathVariable(name = "employeeID") long employeeID) {
         try {
             return "Done";
+        } catch (NoSuchElementException e) {
+            return null;
+        }
+    }
+
+    @GetMapping
+    public String sayHello() {
+        try {
+            return "Hello";
         } catch (NoSuchElementException e) {
             return null;
         }
